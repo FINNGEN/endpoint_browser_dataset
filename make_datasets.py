1ed8423083f6a6ab76d7cba7c16490e233adbbf2
Vincent <vincent-octo@users.noreply.github.com>
2022-12-13 13:59:23 +0000

    add info output with core and meta status

--- make_datasets.py
+++ make_datasets.py
@@ -8,9 +8,17 @@ def main():
     args = init_cli()
 
     path_out_tree = args.output_dir / "endpoint_tree.json"
+    path_out_info = args.output_dir / "endpoints_info.json"
 
     endpoint_tree = build_tree(args.definitions)
     output_tree(endpoint_tree, path_out_tree)
+
+    info = gather_info(
+        args.definitions,
+        args.metaresults_ukbb,
+        args.metaresults_est
+    )
+    output_info(info, path_out_info)
 
 
 def init_cli():
@@ -18,6 +26,18 @@ def init_cli():
     parser.add_argument(
         "-d", "--definitions",
         help="path to FinnGen case and control endpoint definitions (CSV)",
+        required=True,
+        type=Path
+    )
+    parser.add_argument(
+        "-u", "--metaresults-ukbb",
+        help="path to the FinnGen+UKBB metaresults (TSV)",
+        required=True,
+        type=Path
+    )
+    parser.add_argument(
+        "-e", "--metaresults-est",
+        help="path to the FinnGen+EST metaresults (TSV)",
         required=True,
         type=Path
     )
@@ -94,6 +114,47 @@ def output_tree(tree, path):
 
     with open(path, "x") as fd:
         json.dump(out, fd)
+
+
+def gather_info(path_definitions, path_meta_ukbb, path_meta_est):
+    endpoints = {}
+
+    with open(path_definitions) as fd:
+        reader = csv.DictReader(fd)
+
+        for row in reader:
+            endpoint = row["NAME"]
+
+            endpoints[endpoint] = {
+                "endpoint": endpoint,
+                "longname": row["LONGNAME"],
+                "core_endpoint": row["CORE_ENDPOINTS"],
+
+                # placeholders, will be set from other files later on
+                "uk_meta_analysed": "no",
+                "est_meta_analysed": "no"
+            }
+
+
+    set_meta_analysis(endpoints, "uk_meta_analysed", path_meta_ukbb)
+    set_meta_analysis(endpoints, "est_meta_analysed", path_meta_est)
+
+    return endpoints
+
+
+def set_meta_analysis(endpoints, name_meta, path_meta):
+    with open(path_meta) as fd:
+        reader = csv.DictReader(fd, dialect=csv.excel_tab)
+
+        for row in reader:
+            endpoint = row["phenocode"]
+            endpoints[endpoint][name_meta] = "yes"
+
+
+def output_info(data, path):
+    with open(path, "x") as fd:
+        values = list(data.values())
+        json.dump(values, fd)
 
 
 if __name__ == "__main__":
